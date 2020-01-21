070921983ab58fd0c58df1ecd73ef62e69f07915
Alex Wall <saw1848@gmail.com>
2020-01-20 21:41:05 -0600

    backendHandler added. requestHandler renamed to frontendHandler.

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,36 @@
-const functions = require('firebase-functions');
-const express = require('express');
+const functions = require("firebase-functions");
+const express = require("express");
 const app = express();
 
 exports.httpReq = functions.https.onRequest(app);
 
-app.get('/', requestHandler);
-app.get('/home', requestHandler);
-app.get('/login', requestHandler);
+app.get("/", frontendHandler);
+app.get("/home", frontendHandler);
+app.get("/login", frontendHandler);
+app.get("/shmoodle", frontendHandler);
 
-function requestHandler(request, response) {
+app.get('/b', backendHandler)
+
+function frontendHandler(request, response) {
     response.sendFile(__dirname + "/spa/index.html");
+}
+
+function backendHandler(request, response){
+    const n = Math.random();
+    const num = Math.floor(n*10);
+    //in c++, I'd just have 'int s, p, d, q;'
+    let s = 0; let p = 0; let d = 0; let q = 0;
+    s = 36 + 5;
+    p = 36 * 5;
+    d = 36 - 5;
+    q = 36/5;
+    let page = `
+        <h1>Your Lucky Number is ${num}</h1>
+        36 and 5!!!!!!
+        sum  is ${s}
+        product is ${p}
+        quotient is ${q}
+    `;
+
+    response.send(page)
 }
