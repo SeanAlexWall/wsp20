52dd4a289784e8e860b8d6407a359b2aef9e34ae
Alex Wall <saw1848@gmail.com>
2020-01-27 21:20:49 -0600

    Added Rock Paper Scissors on /rps and a page with a button that rickrolls you on /rickroll

--- functions/spa/js/login_page.js
+++ functions/spa/js/login_page.js
@@ -1,7 +1,7 @@
 function login_page(){
     pageContent.innerHTML = `
 <form>
-  l<div class="form-group">
+  <div class="form-group">
     <label for="exampleInputEmail1">Email address</label>
     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
---
+++ functions/spa/js/rps.js
@@ -0,0 +1,90 @@
+function rps(choice){
+    pageContent.innerHTML = `<h1>Rock, Paper, Scissors</br></h1>`;
+    
+    if(choice === 0){
+        pageContent.innerHTML += `
+        <p>Choose one!</p>
+        <a class="btn btn-secondary" href="/rps1" role="button">Rock</a>
+        <a class="btn btn-success" href="/rps2" role="button">Paper</a>
+        <a class="btn btn-primary" href="/rps3" role="button">Scissors</a>
+        <br>
+        `;
+    }
+    else if(choice > 0 && choice < 4){
+        let outcome = ""
+        let n = Math.random();
+        n = Math.floor(n*1000);
+        n = (n%3)+1;
+        console.log(n);
+        outcome = decide(choice, n);
+
+
+        switch(outcome){
+            case "won":
+                pageContent.innerHTML += "<h2 class='text-success'> You Won!</h2><br>"
+                break;
+            case "lost":
+                pageContent.innerHTML += "<h2 class='text-danger'>You lost!</h2><br>"
+                break;
+            case "tie":
+                pageContent.innerHTML += "<h2 class='text-primary'>It's A Tie!</h2><br>"
+                break;
+            default:
+                pageContent.innerHTML += "lkadsfjhlafhawliuh<br>"
+                break;
+        }
+        let compChoice = displayChoice(n);
+        let humChoice = displayChoice(choice);
+    
+        pageContent.innerHTML += `
+        <p class='text-dark'>
+        You chose ${humChoice}!<br>
+        I chose ${compChoice}!<br>
+        <p><br>
+        <a class="btn btn-primary" href="/rps" role="button">Try Again?</a>
+        `
+    }
+}
+
+function decide(choice, n){
+
+    if(n === 1){
+        if(choice === 1){
+            return "tie";
+        }
+        else if(choice === 2){
+            return "won";
+        }
+        else if(choice == 3){
+            return "lost";
+        }
+    }
+    else if(n === 2){
+        if(choice === 1){
+            return "lost";
+        }
+        else if(choice === 2){
+            return "tie";
+        }
+        else if(choice == 3){
+            return "won";
+        }
+    }
+    else if(n === 3){
+        if(choice === 1){
+            return "won";
+        }
+        else if(choice === 2){
+            return "lost";
+        }
+        else if(choice == 3){
+            return "tie";
+        }
+    }
+}
+
+function displayChoice(choice){
+    if(choice === 1) return "Rock";
+    if(choice === 2) return "Paper";
+    if(choice === 3) return "Scissors";
+}
